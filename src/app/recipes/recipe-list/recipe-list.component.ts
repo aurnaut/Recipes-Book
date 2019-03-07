8865427d2705d574494ea0dfea3eb988840d1e6b
unknown <marius.arnautu@gmail.com>
2019-03-07 15:14:41 +0000

    pass data from recipe list to recipe detail and display it on click event

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.merchantsmarket.com/wp-content/uploads/2018/04/recipe-icon-300x300.png'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.merchantsmarket.com/wp-content/uploads/2018/04/recipe-icon-300x300.png')
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://www.merchantsmarket.com/wp-content/uploads/2018/04/recipe-icon-300x300.png"
+    ),
+    new Recipe(
+      "Another Test Recipe",
+      "This is simply a test",
+      "https://www.merchantsmarket.com/wp-content/uploads/2018/04/recipe-icon-300x300.png"
+    )
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  selector: "app-recipes",
+  templateUrl: "./recipes.component.html",
+  styleUrls: ["./recipes.component.css"]
 })
 export class RecipesComponent implements OnInit {
-
+  selectedRecipe: Recipe;
   recipes = [];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
