695a1ac453ea414e387b936930bd2650b97284ae
unknown <marius.arnautu@gmail.com>
2019-03-11 12:41:29 +0000

    add ingredients to the model and service of Recipe and pass it down onto the Recipe Detail component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://www.merchantsmarket.com/wp-content/uploads/2018/04/recipe-icon-300x300.png"
+      "Tasty Schnitzel",
+      "A super-tasty Schnitzel - just awesome!",
+      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
     ),
     new Recipe(
-      "Another Test Recipe",
-      "This is simply a test",
-      "https://www.merchantsmarket.com/wp-content/uploads/2018/04/recipe-icon-300x300.png"
+      "Big Fat Burger",
+      "What else you need to say?",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     )
   ];
 
